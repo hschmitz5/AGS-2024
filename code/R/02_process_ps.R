9f21936b8ed5af0904e0b6b618208240d1dc7f83
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-02 13:36:19 -0500

    cleaned

--- code/R/02_process_ps.R
+++ code/R/02_process_ps.R
@@ -15,25 +15,3 @@ get_rel_ASV <- function(ps) {
   
   phyloseq::psmelt(ps_rel_ASV)
 }
-
-
-# get_rel_genus <- function(ps) {
-#   metadata <- get_metadata(ps)
-#   taxonomy <- get_taxonomy(ps)
-#   
-#   # define relative abundance
-#   ps_rel_ASV <- phyloseq::transform_sample_counts(ps, function(x) x*100/sum(x))
-#   
-#   table_rel <- as.data.frame(as.matrix(ps_rel_ASV@otu_table)) %>%
-#     rownames_to_column(var = "OTU")
-#   
-#   table_rel_long <- table_rel %>%
-#     pivot_longer(cols = !OTU, names_to = "Sample", values_to = "tmp_abund") %>% 
-#     left_join(taxonomy, join_by(OTU)) %>%
-#     left_join(metadata, join_by(Sample)) %>%
-#     group_by(Sample, size.name, size.mm, Genus) %>%
-#     # Adds multiple ASVs in the same Genus
-#     summarise(Abundance = sum(tmp_abund), .groups = "drop")
-#   
-#   return(table_rel_long)
-# }
