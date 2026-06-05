c200eca66d63f6259fd9fd95745e4eeb7d9015c8
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-05 14:40:29 -0500

    changed source

--- code/R/generate_data/write_metabolism_genera.R
+++ code/R/generate_data/write_metabolism_genera.R
@@ -1,16 +1,8 @@
 rm(list = ls())
 library(writexl)
-source("./code/R/00_setup.R")
-source("./code/R/01_load_data.R")
-source("./code/R/02_process_ps.R")
-source("./code/R/03_diff_ab.R")
-source("./code/R/04_DA_agglom.R")
+source("./code/R/01_load_ps.R")
 
 fname_out <- "./data/midas_genera.xlsx"
-
-#### Rename: agglomerate names when multiple ASVs are differentially abundant or not
-ancom_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold, rel_ab_cutoff, write2excel = FALSE)
-DA_taxa <- ancom_taxa$high_ab 
 
 # define taxa in which at least one sample has abundance > rel_ab_cutoff
 high_ab_taxa <- get_rel_ASV(ps) %>%
