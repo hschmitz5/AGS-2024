a3913f69a9733fa7e18ecd4e8203049051db8c89
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-09 10:07:39 -0500

    not agglomerating ASVs

--- code/R/02_metab_and_DA.R
+++ code/R/02_metab_and_DA.R
@@ -1,19 +1,22 @@
 library(readxl)
 
 # ANCOM-BC2 differential abundance data
-ancom_fname <- "./data/ancombc2_ASV.rds"
+ancom_fname <- "./data/ancombc2_genus.rds"
 # Metabolism input file
 metab_fname <- "./data/metabolism_midas.xlsx"
 
+# p-value used for defining significant taxa in differential abundance
+p_threshold <- 0.05
+
+# input df must contain column named Genus
 get_metabolism <- function(df, metab_fname) {
   m <- read_excel(metab_fname, sheet = "input") # tibble
   
   metab <- df %>%
-    dplyr::select(Genus, OTU) %>%
+    dplyr::select(Genus) %>%
     distinct() %>%
     left_join(., m, by = "Genus") %>%
-    dplyr::select(-Genus) %>%
-    column_to_rownames("OTU")
+    column_to_rownames("Genus")
 }
 
 get_ancom_taxa <- function(fname_in, ps, p_threshold) {
@@ -26,7 +29,6 @@ get_ancom_taxa <- function(fname_in, ps, p_threshold) {
   all_sig_taxa <- output$res %>%
     rename(OTU = taxon) %>%
     left_join(taxonomy, join_by(OTU)) %>%
-    filter(!is.na(Phylum)) %>%
     # Combines diff_size* and passed_ss* together
     pivot_longer(
       cols = matches("q_size\\.name|passed_ss_size\\.name"),
@@ -34,60 +36,8 @@ get_ancom_taxa <- function(fname_in, ps, p_threshold) {
       names_pattern = "(q|passed_ss)_size\\.name(.*)"
     ) %>%
     filter(q < p_threshold & passed_ss == TRUE) %>%   
-    distinct(OTU) %>%
-    pull(OTU)
-}
-
-get_rel_agglom <- function(ps, ancom_fname, rel_ab_cutoff, p_threshold) { 
-  # define taxa in which at least one sample has abundance > rel_ab_cutoff
-  high_ab_taxa <- get_rel_ASV(ps) %>%
-    filter(Abundance > rel_ab_cutoff) %>%
-    distinct(OTU) %>%
-    pull(OTU)
-  
-  # define differentially abundant (DA) taxa
-  DA_taxa <- get_ancom_taxa(ancom_fname, ps, p_threshold)
-  
-  # define relative abundance
-  rel_wide <- get_rel_ASV(ps) %>%
-    filter(OTU %in% high_ab_taxa) %>%
-    dplyr::select(Genus, OTU, Sample, Abundance) %>%  
-    pivot_wider(
-      names_from = Sample,
-      values_from = Abundance
-    ) %>%
-    dplyr::select(Genus, OTU, all_of(sam_name)) %>%
-    mutate(
-      DA = ifelse(OTU %in% DA_taxa, "T", "F")
-    )
-  
-  # agglomerate significant and insignificant taxa separately
-  data_mat <- rel_wide %>%
-    # define name_prefix - just the core OTU name without "-#" at the end
-    mutate(
-      name_prefix = if_else(
-        grepl("[0-9]$", OTU),       # last character is a number
-        sub("-[^-]+$", "", OTU),    # remove last dash and everything after
-        OTU                         # last character is a letter → keep full OTU
-      )
-    ) %>%
-    # sum relative abundance of groups with same name_prefix and DA
-    group_by(DA, Genus, name_prefix) %>%
-    summarise(
-      n = n(), # number of rows in group
-      OTU_orig = first(OTU),  # only used when n = 1
-      across(all_of(sam_name), \(x) sum(x, na.rm = TRUE)),
-      .groups = "drop"
-    ) %>%
-    # create new name
-    mutate(
-      OTU = if_else(
-        n == 1,
-        OTU_orig,
-        paste0(name_prefix, "_", DA)
-      )
-    ) %>%
-    select(Genus, OTU, DA, all_of(sam_name))
+    distinct(Genus) %>%
+    pull(Genus)
 }
 
 # Only the difference between bias corrected data is meaningful.
