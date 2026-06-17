3ffe1e4105a4b3e71491c2a25bd1dffff96dd485
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-17 12:53:29 -0500

    turned off struc zero and specified alpha

--- code/R/generate_data/run_ancombc2.R
+++ code/R/generate_data/run_ancombc2.R
@@ -1,90 +1,75 @@
 rm(list = ls())
 
+library(tidyverse)
 library(phyloseq)
 library(ANCOMBC)
 
-fname_P <- c("./data/phyloseq/ps_metab_P.rds", "./data/DA/ancombc2_metab_P.rds")
-fname_PV <- c("./data/phyloseq/ps_metab_PV.rds", "./data/DA/ancombc2_metab_PV.rds")
+fname_out <- "./data/DA/ancombc2_genus.rds"
 
-run_ancom <- function(fname_in, fname_out) {
-  ps <- readRDS(fname_in) 
+ps <- readRDS("./data/phyloseq/ps_genus_subset.rds") 
   
-  # Keep taxa present in at least three samples
-  keep_prev <- rowSums(otu_table(ps) > 0) >= 3
-  # Keep taxa with at least 10 total reads across all samples
-  keep_abund <- taxa_sums(ps) >= 10
-  # Apply conditions
-  ps_filt <- prune_taxa(keep_prev & keep_abund, ps)
+set.seed(123)
+output <- ancombc2(
+  data = ps,
+  prv_cut = 0.1, # Removes taxa not present in 10% of samples
+  fix_formula = "size.name",    
+  group = "size.name",
+  alpha = 0.05,
+  global = TRUE, dunnet = TRUE, 
+  pairwise = FALSE, trend = FALSE,
+  struc_zero = FALSE
+)
   
-  set.seed(123)
-  output <- ancombc2(
-    data = ps_filt,
-    fix_formula = "size.name",    
-    group = "size.name",
-    struc_zero = TRUE,
-    global = TRUE, dunnet = TRUE, 
-    pairwise = FALSE, trend = FALSE   
-  )
-  
-  saveRDS(output, file = fname_out)
-  
-  return(output)
-}
+saveRDS(output, file = fname_out)
 
-output_P <- run_ancom(fname_P[1], fname_P[2])
-output_PV <- run_ancom(fname_PV[1], fname_PV[2])
 
 # ------ Process Data ------
 
-process_DA <- function(output, value_col) {
-  res_prim = output$res_dunn %>% # or res
-    rename(metab = taxon)
-  
-  df_lfc <- res_prim %>%
-    # set non-DA values to zero
-    dplyr::mutate(M = ifelse(diff_size.nameM == 1, 
-                             round(lfc_size.nameM, 2), 0),
-                  L = ifelse(diff_size.nameL == 1, 
-                             round(lfc_size.nameL, 2), 0),
-                  XL = ifelse(diff_size.nameXL == 1, 
-                              round(lfc_size.nameXL, 2), 0),
-                  XXL = ifelse(diff_size.nameXXL == 1, 
-                               round(lfc_size.nameXXL, 2), 0)) %>%
-    dplyr::select(metab, M, L, XL, XXL) %>%
-    tidyr::pivot_longer(!metab, names_to = "size", values_to = "lfc") %>%
-    dplyr::arrange(metab)
-  
-  df_robust <- res_prim %>%
-    # filter for values that are differentially abundant (TRUE)
-    dplyr::filter(diff_size.nameM == 1 | diff_size.nameL == 1 |
-                    diff_size.nameXL == 1 | diff_size.nameXXL == 1) %>%
-    # true if robust (is differentially abundant and passed sensitivity analysis)
-    dplyr::mutate(M = ifelse(diff_robust_size.nameM == 1, 
-                             TRUE, FALSE),
-                  L = ifelse(diff_robust_size.nameL == 1, 
-                             TRUE, FALSE),
-                  XL = ifelse(diff_robust_size.nameXL == 1, 
-                              TRUE, FALSE),
-                  XXL = ifelse(diff_robust_size.nameXXL == 1, 
-                               TRUE, FALSE)) %>%
-    dplyr::select(metab, M, L, XL, XXL) %>%
-    tidyr::pivot_longer(!metab, names_to = "size", values_to = "robust") %>%
-    dplyr::arrange(metab)
-  
-  df = df_lfc %>%
-    dplyr::full_join(df_robust, by = c("metab", "size")) %>%
-    mutate(size = factor(size, levels = c("M", "L", "XL", "XXL"))) %>%
-    mutate(metab_val = value_col)
-  
-  return(df)
-}
+taxonomy <- data.frame(tax_table(ps)) %>%
+  tibble::rownames_to_column("OTU") %>%
+  dplyr::select(OTU, Genus)
 
-# Make sure lfc_P and lfc_PV have the same metab groups defined
-lfc_P <- process_DA(output_P, "Positive") %>%
-  filter(!is.na(robust))
+res_prim = output$res_dunn %>%
+  rename(OTU = taxon) %>%
+  left_join(taxonomy, by = "OTU")
+  
+df_lfc <- res_prim %>%
+  # filter for values that are differentially abundant (TRUE)
+  dplyr::filter(diff_size.nameM == 1 | diff_size.nameL == 1 |
+                  diff_size.nameXL == 1 | diff_size.nameXXL == 1) %>%
+  # set non-DA values to zero
+  dplyr::mutate(M = ifelse(diff_size.nameM == 1, 
+                            round(lfc_size.nameM, 2), 0),
+                L = ifelse(diff_size.nameL == 1, 
+                            round(lfc_size.nameL, 2), 0),
+                XL = ifelse(diff_size.nameXL == 1, 
+                            round(lfc_size.nameXL, 2), 0),
+                XXL = ifelse(diff_size.nameXXL == 1, 
+                              round(lfc_size.nameXXL, 2), 0)) %>%
+  dplyr::select(Genus, M, L, XL, XXL) %>%
+  tidyr::pivot_longer(!Genus, names_to = "size", values_to = "lfc") %>%
+  dplyr::arrange(Genus)
+  
+df_robust <- res_prim %>%
+  # filter for values that are differentially abundant (TRUE)
+  dplyr::filter(diff_size.nameM == 1 | diff_size.nameL == 1 |
+                  diff_size.nameXL == 1 | diff_size.nameXXL == 1) %>%
+  # true if robust (is differentially abundant and passed sensitivity analysis)
+  dplyr::mutate(M = ifelse(diff_robust_size.nameM == 1, 
+                           TRUE, FALSE),
+                L = ifelse(diff_robust_size.nameL == 1, 
+                           TRUE, FALSE),
+                XL = ifelse(diff_robust_size.nameXL == 1, 
+                            TRUE, FALSE),
+                XXL = ifelse(diff_robust_size.nameXXL == 1, 
+                             TRUE, FALSE)) %>%
+  dplyr::select(Genus, M, L, XL, XXL) %>%
+  tidyr::pivot_longer(!Genus, names_to = "size", values_to = "robust") %>%
+  dplyr::arrange(Genus)
+  
+df = df_lfc %>%
+  dplyr::full_join(df_robust, by = c("Genus", "size")) %>%
+  mutate(size = factor(size, levels = c("M", "L", "XL", "XXL"))) 
 
-lfc_PV <- process_DA(output_PV, "Positive + Variable") 
 
-lfc_full <- bind_rows(lfc_P, lfc_PV) 
-
-saveRDS(lfc_full, file = "./data/DA/DA_metab_processed.rds")
+saveRDS(df, file = "./data/DA/DA_genus_processed.rds")
---
+++ code/R/generate_data/run_ancombc2_metab.R
@@ -0,0 +1,86 @@
+rm(list = ls())
+
+library(tidyverse)
+library(phyloseq)
+library(ANCOMBC)
+
+fname_P <- c("./data/phyloseq/ps_metab_P.rds", "./data/DA/ancombc2_metab_P.rds")
+fname_PV <- c("./data/phyloseq/ps_metab_PV.rds", "./data/DA/ancombc2_metab_PV.rds")
+
+run_ancom <- function(fname_in, fname_out) {
+  ps <- readRDS(fname_in) 
+  
+  set.seed(123)
+  output <- ancombc2(
+    data = ps,
+    prv_cut = 0.1, # Removes taxa not present in 10% of samples
+    fix_formula = "size.name",    
+    group = "size.name",
+    alpha = 0.05,
+    global = TRUE, dunnet = TRUE, 
+    pairwise = FALSE, trend = FALSE,
+    struc_zero = FALSE
+  )
+  
+  saveRDS(output, file = fname_out)
+  
+  return(output)
+}
+
+output_P <- run_ancom(fname_P[1], fname_P[2])
+output_PV <- run_ancom(fname_PV[1], fname_PV[2])
+
+# ------ Process Data ------
+
+process_DA <- function(output, value_col) {
+  res_prim = output$res_dunn %>% # or res
+    rename(metab = taxon)
+  
+  df_lfc <- res_prim %>%
+    # set non-DA values to zero
+    dplyr::mutate(M = ifelse(diff_size.nameM == 1, 
+                             round(lfc_size.nameM, 2), 0),
+                  L = ifelse(diff_size.nameL == 1, 
+                             round(lfc_size.nameL, 2), 0),
+                  XL = ifelse(diff_size.nameXL == 1, 
+                              round(lfc_size.nameXL, 2), 0),
+                  XXL = ifelse(diff_size.nameXXL == 1, 
+                               round(lfc_size.nameXXL, 2), 0)) %>%
+    dplyr::select(metab, M, L, XL, XXL) %>%
+    tidyr::pivot_longer(!metab, names_to = "size", values_to = "lfc") %>%
+    dplyr::arrange(metab)
+  
+  df_robust <- res_prim %>%
+    # filter for values that are differentially abundant (TRUE)
+    dplyr::filter(diff_size.nameM == 1 | diff_size.nameL == 1 |
+                    diff_size.nameXL == 1 | diff_size.nameXXL == 1) %>%
+    # true if robust (is differentially abundant and passed sensitivity analysis)
+    dplyr::mutate(M = ifelse(diff_robust_size.nameM == 1, 
+                             TRUE, FALSE),
+                  L = ifelse(diff_robust_size.nameL == 1, 
+                             TRUE, FALSE),
+                  XL = ifelse(diff_robust_size.nameXL == 1, 
+                              TRUE, FALSE),
+                  XXL = ifelse(diff_robust_size.nameXXL == 1, 
+                               TRUE, FALSE)) %>%
+    dplyr::select(metab, M, L, XL, XXL) %>%
+    tidyr::pivot_longer(!metab, names_to = "size", values_to = "robust") %>%
+    dplyr::arrange(metab)
+  
+  df = df_lfc %>%
+    dplyr::full_join(df_robust, by = c("metab", "size")) %>%
+    mutate(size = factor(size, levels = c("M", "L", "XL", "XXL"))) %>%
+    mutate(metab_val = value_col)
+  
+  return(df)
+}
+
+# Make sure lfc_P and lfc_PV have the same metab groups defined
+lfc_P <- process_DA(output_P, "Positive") %>%
+  filter(!is.na(robust))
+
+lfc_PV <- process_DA(output_PV, "Positive + Variable") 
+
+lfc_full <- bind_rows(lfc_P, lfc_PV) 
+
+saveRDS(lfc_full, file = "./data/DA/DA_metab_processed.rds")
