970517cad7f174fc5b37cdb9668bc6f88fc23543
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-06-24 11:35:13 -0500

    added summary

--- code/R/plot_modulus.R
+++ code/R/plot_modulus.R
@@ -1,64 +1,78 @@
 rm(list = ls())
-source("./code/R/00_setup.R")
-source("./code/R/01_load_data.R")
 library(readxl)
 library(tidyverse)
 library(patchwork)
 library(MetBrewer)
-
-fname_in  <- "./data/Rheometry_Nov_2024.xlsx"
-fname_out <- "./figures/moduli.png"
+source("./code/R/01_load_ps.R")
 
 # define sample names
-size <- data.frame(
+sz <- data.frame(
   name = c("S", "M", "L", "XL", "XXL")
 )
 
+fname_in <- "./data/Rheometry_Nov_2024.xlsx"
+modulus <- read_excel(fname_in, sheet = "input", skip = 1) %>%
+  filter(size != "XS") %>%
+  select(size, freq_rad, G_avg, G_sd, G2_avg, G2_sd) %>%
+  pivot_longer(
+    cols = c(G_avg, G_sd, G2_avg, G2_sd),
+    names_to = c("measure", ".value"),
+    names_pattern = "(G2?|G2?)_(avg|sd)"
+  ) %>%
+  mutate(
+    size = factor(size, levels = sz$name),
+    measure = factor(measure, levels = c("G", "G2")),
+    measure = recode(measure,"G"="Storage Modulus (G')","G2"='Loss Modulus (G")')
+    )
+
+modulus_subset <- modulus %>%
+  filter(freq_rad == 0.1) %>%
+  select(-freq_rad) 
+
 #### Plot
 
-p1 <- ggplot(modulus, aes(x = freq_rad, y = G_avg, color = size)) +
+p <- ggplot(modulus, aes(x = freq_rad, y = avg, color = size)) +
   geom_point() +
   geom_line(aes(group = size)) +
   geom_errorbar(
-    aes(ymin = pmax(G_avg - G_sd, 0), ymax = G_avg + G_sd),
+    aes(ymin = pmax(avg - sd, 0), ymax = avg + sd),
     width = 0.2
   ) +
+  facet_wrap(~measure, scales = "free_y", nrow = 1) +
   scale_color_manual(
     name = "Size", 
     values = met.brewer(size_pal, n_sizes)
   ) +
   labs(
-    x = "Frequency [rad/s]",
-    y = "Storage Modulus [Pa]",
-  ) 
-
-p2 <- ggplot(modulus, aes(x = freq_rad, y = G2_avg, color = size)) +
-  geom_point() +
-  geom_line(aes(group = size)) +
-  geom_errorbar(
-    aes(ymin = pmax(G2_avg - G2_sd, 0), ymax = G2_avg + G2_sd),
-    width = 0.2
+    x = "Frequency (rad/s)",
+    y = "Modulus (Pa)",
   ) +
-  scale_color_manual(
-    name = "Size",
-    values = met.brewer(size_pal, n_sizes)
+  theme_minimal(base_size = 12) +
+  theme(legend.position = "bottom")
+
+fname_out <- "./figures/moduli.png"
+ggsave(fname_out, plot = p, width = 6.5, height = 3, dpi = 300)
+
+
+p_sub <- ggplot(modulus_subset, aes(x = size, y = avg, fill = measure)) +
+  geom_col(position = "dodge", width = 0.6) +
+  geom_errorbar(
+    aes(ymin = avg - sd, ymax = avg + sd),
+    width = 0.2,
+    position = position_dodge(width = 0.6)
   ) +
   labs(
-    x = "Frequency [rad/s]",
-    y = "Loss Modulus [Pa]",
-  ) 
-
-
-# horizontal
-p <- p1 + p2 + 
-  plot_layout(guides = "collect") & 
+    title = "Frequency = 0.1 rad/s",
+    x = "Size",
+    y = "Modulus (Pa)"
+  ) +
+  scale_fill_manual(
+    values = c("#765DA0", "lightgray")
+  ) +
   theme_minimal(base_size = 12) +
-  theme(legend.position = "bottom") &
-  guides(
-    color = guide_legend(
-      title.position = "bottom",
-      title.hjust = 0.5 # centers title
-      )
+  theme(
+    legend.title = element_blank()
   )
 
-ggsave(fname_out, plot = p, width = 8, height = 3, dpi = 600)
+fname_out <- "./figures/moduli_subset.png"
+ggsave(fname_out, plot = p_sub, width = 6.5, height = 3, dpi = 300)
