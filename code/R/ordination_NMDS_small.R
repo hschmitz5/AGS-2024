3cb922dae399df89059c8df84ff9258b0a022dc9
hschmitz5 <130790347+hschmitz5@users.noreply.github.com>
2026-08-03 12:37:57 -0500

    remove stress text

--- code/R/ordination_NMDS_small.R
+++ code/R/ordination_NMDS_small.R
@@ -26,9 +26,6 @@ nmds_df <- scores(nmds) %>%
   as_tibble(rownames = "SampleID") %>%
   left_join(., metadata, by = "SampleID")
 
-stress_text <- paste0("2D Stress = ",round(nmds$stress,3))
-print(stress_text)
-
 # ------ Plot ------
 
 # symbol
